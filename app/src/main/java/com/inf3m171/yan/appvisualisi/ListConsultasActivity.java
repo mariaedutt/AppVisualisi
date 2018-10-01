7ba0fd7694d61204282770331c7647d92004bfa0
Maria Eduarda Brito de Souza <maria_edutt@hotmail.com>
2018-10-01 14:51:14 -0300

    Atualização da Maria, 01-10

--- app/src/main/java/com/inf3m171/yan/appvisualisi/ListConsultasActivity.java
+++ app/src/main/java/com/inf3m171/yan/appvisualisi/ListConsultasActivity.java
@@ -4,15 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
-import java.util.List;
-
-public class ListExamesActivity extends AppCompatActivity {
+public class ListConsultasActivity extends AppCompatActivity {
 
     private ListView listaExames;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_exames);
+        setContentView(R.layout.activity_list_consultas);
 
         listaExames = (ListView) findViewById(R.id.lvListaExames);
     }
