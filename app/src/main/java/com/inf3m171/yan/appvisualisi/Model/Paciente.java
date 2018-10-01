bf83d7510ccb9da2791de2f3a2da99d77d46be8a
tvvieira <bragafe2010@gmail.com>
2018-10-01 16:56:13 -0300

    Alterações vladi 01.10

--- app/src/main/java/com/inf3m171/yan/appvisualisi/Model/Paciente.java
+++ app/src/main/java/com/inf3m171/yan/appvisualisi/Model/Paciente.java
@@ -5,8 +5,61 @@ package com.inf3m171.yan.appvisualisi.Model;
  */
 
 public class Paciente {
-    private String email;
-    private int senha;
+    private String nome, cpf, codigo, dataDeNascimento, telefone, email, senha, confirmaSenha ;
+
+    public Paciente() {
+    }
+
+    public Paciente(String nome, String cpf, String codigo, String dataDeNascimento, String telefone, String email, String senha, String confirmaSenha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.codigo = codigo;
+        this.dataDeNascimento = dataDeNascimento;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
+        this.confirmaSenha = confirmaSenha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 
     public String getEmail() {
         return email;
@@ -16,11 +69,23 @@ public class Paciente {
         this.email = email;
     }
 
-    public int getSenha() {
+    public String getSenha() {
         return senha;
     }
 
-    public void setSenha(int senha) {
+    public void setSenha(String senha) {
         this.senha = senha;
     }
+
+    public String getConfirmaSenha() {
+        return confirmaSenha;
+    }
+
+    public void setConfirmaSenha(String confirmaSenha) {
+        this.confirmaSenha = confirmaSenha;
+    }
+
+
 }
+
+
